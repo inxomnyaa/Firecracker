904e5a875eae4ed5bb379780addd066ef2f4c00d
XenialDan <thebigsmilexd@gmail.com>
2020-12-24 16:20:02 +0100

    Update to PM4

--- src/xenialdan/Firecracker/Loader.php
+++ src/xenialdan/Firecracker/Loader.php
@@ -19,32 +19,32 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
 use pocketmine\item\ItemIds;
-use pocketmine\level\particle\ExplodeParticle;
-use pocketmine\level\particle\FlameParticle;
-use pocketmine\level\sound\FizzSound;
 use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\utils\TextFormat;
+use pocketmine\world\particle\ExplodeParticle;
+use pocketmine\world\particle\FlameParticle;
+use pocketmine\world\sound\FizzSound;
 
 class Loader extends PluginBase implements Listener
 {
-    /** @var Item */
-    public static $brick;
+	/** @var Item */
+	public static $brick;
 
-    public function onEnable()
-    {
-        $this->makeSaveFiles();
-        $item = ItemFactory::get(ItemIds::BRICK);
-        $item->setCustomName($this->getConfig()->get("translation", "Firecracker"));
+	public function onEnable()
+	{
+		$this->makeSaveFiles();
+		$item = ItemFactory::getInstance()->get(ItemIds::BRICK);
+		$item->setCustomName($this->getConfig()->get("translation", "Firecracker"));
         $item->setLore(["Drop me and i will explode!"]);
         self::$brick = $item;
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         #$this->getLogger()->info(TextFormat::GOLD . "Happy new Year! Be safe!");
-        $this->getScheduler()->scheduleDelayedRepeatingTask(new ClosureTask(function (int $currentTick): void {
-            $this->makeParticleSound();
-        }), 20, 20);
+		$this->getScheduler()->scheduleDelayedRepeatingTask(new ClosureTask(function (): void {
+			$this->makeParticleSound();
+		}), 20, 20);
     }
 
     private function makeSaveFiles(): void
@@ -74,17 +74,21 @@ class Loader extends PluginBase implements Listener
     }
 
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
-    {
-        switch ($command->getName()) {
-            case "firecracker":
-            {
-                if (count($args) > 0) {
-                    switch ($args[0]) {
-                        case "get":
-                        {
-                            if ($this->runIngame($sender)) {
-                                $this->giveFirecracker($sender);
-                                return true;
+	{
+		if (!$sender instanceof Player) {
+			$sender->sendMessage(TextFormat::RED . "This command is only for players");
+			return true;
+		}
+		switch ($command->getName()) {
+			case "firecracker":
+			{
+				if (count($args) > 0) {
+					switch ($args[0]) {
+						case "get":
+						{
+							if ($this->runIngame($sender)) {
+								$this->giveFirecracker($sender);
+								return true;
                             }
                         }
                         default:
@@ -101,9 +105,9 @@ class Loader extends PluginBase implements Listener
     public function onJoin(PlayerJoinEvent $event): void
     {
         $player = $event->getPlayer();
-        $this->getScheduler()->scheduleDelayedTask(new ClosureTask(function (int $currentTick) use ($player) : void {
-            $this->giveFirecracker($player);
-        }), 20);
+		$this->getScheduler()->scheduleDelayedTask(new ClosureTask(function () use ($player): void {
+			$this->giveFirecracker($player);
+		}), 20);
     }
 
     public function onDrop(PlayerDropItemEvent $event): void
@@ -111,9 +115,9 @@ class Loader extends PluginBase implements Listener
         $item = $event->getItem();
         if ($item->equals(self::$brick)) {
             $player = $event->getPlayer();
-            $this->getScheduler()->scheduleDelayedTask(new ClosureTask(function (int $currentTick) use ($player): void {
-                $this->giveFirecracker($player);
-            }), $this->getConfig()->get("give-items-after", 30) * 20);
+			$this->getScheduler()->scheduleDelayedTask(new ClosureTask(function () use ($player): void {
+				$this->giveFirecracker($player);
+			}), $this->getConfig()->get("give-items-after", 30) * 20);
         }
     }
 
@@ -129,39 +133,37 @@ class Loader extends PluginBase implements Listener
     }
 
     public function giveFirecracker(Player $player): void
-    {
-        if ($player->isOnline()) {
-            if (!$player->getInventory()->contains(self::$brick)) {
-                $player->getInventory()->addItem(self::$brick);
-                $player->sendWhisper($this->getDescription()->getPrefix(), "You got a new firecracker!");
-            }
-        }
-    }
+	{
+		if ($player->isOnline() && !$player->getInventory()->contains(self::$brick)) {
+			$player->getInventory()->addItem(self::$brick);
+			$player->sendActionBarMessage(TextFormat::RED . "You got a new firecracker!");
+		}
+	}
 
     public function makeParticleSound(): void
-    {
-        foreach ($this->getServer()->getLevels() as $level) {
-            if ($level->isClosed() || count($level->getPlayers()) <= 0) continue;
-            foreach ($level->getEntities() as $entity) {
-                if (!$entity instanceof ItemEntity) continue;
-                if ($entity->getItem()->equals(self::$brick)) {
-                    if ($entity->ticksLived >= 60) {
-                        $this->explodeFirecracker($entity);
-                        continue;
-                    }
-                    $entity->getLevel()->addSound(new FizzSound($entity->asVector3()));
-                    $entity->getLevel()->addParticle(new FlameParticle($entity->add(0, 0.5)));
-                }
-            }
+	{
+		foreach ($this->getServer()->getWorldManager()->getWorlds() as $world) {
+			if ($world->isClosed() || count($world->getPlayers()) <= 0) continue;
+			foreach ($world->getEntities() as $entity) {
+				if (!$entity instanceof ItemEntity) continue;
+				if ($entity->getItem()->equals(self::$brick)) {
+					if ($entity->ticksLived >= 60) {
+						$this->explodeFirecracker($entity);
+						continue;
+					}
+					$entity->getWorld()->addSound($entity->getPosition()->asVector3(), new FizzSound());
+					$entity->getWorld()->addParticle($entity->getPosition()->add(0, 0.5, 0), new FlameParticle());
+				}
+			}
         }
     }
 
     public function explodeFirecracker(ItemEntity $itementity): void
-    {
-        if (!$itementity->isClosed() && $itementity->isValid()) {
-            $itementity->getLevel()->addParticle(new ExplodeParticle($itementity->asVector3()));
-            $itementity->getLevel()->broadcastLevelSoundEvent($itementity->asVector3(), LevelSoundEventPacket::SOUND_EXPLODE);
-            $itementity->flagForDespawn();
-        }
-    }
+	{
+		if (!$itementity->isClosed() && !$itementity->getItem()->isNull()) {
+			$itementity->getWorld()->addParticle($itementity->getPosition()->asVector3(), new ExplodeParticle());
+			$itementity->getWorld()->broadcastPacketToViewers($itementity->getPosition()->asVector3(), LevelSoundEventPacket::create(LevelSoundEventPacket::SOUND_EXPLODE, $itementity->getPosition()->asVector3()));
+			$itementity->flagForDespawn();
+		}
+	}
 }
